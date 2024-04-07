dd7345e3fef2bb46ce230e11939359b9680c04fa
alek <wait82985@gmail.com>
2024-04-07 15:27:22 +0500

    O(n) solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,42 +1,60 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
     public boolean checkValidString(String s) {
-        Boolean[][] memo = new Boolean[s.length() + 1][s.length() + 1];
-        return dfs(s, 0, 0, memo);
-    }
-    boolean dfs(String s, int i, int count, Boolean[][] memo){
-        if(i == s.length()){
-            return count == 0;
-        }
-        if(memo[i][count] != null){
-            return memo[i][count];
-        }
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        Queue<Integer> q = new LinkedList<>();
+        int open = 0;
 
+        for (int i = 0; i < n; i++) {
+            char ch = sb.charAt(i);
+            if (ch == '(') {
+                open++;
+            } else if (ch == '*') {
+                q.add(i);
+            } else {
+                if (open == 0 && q.isEmpty()) {
+                    return false;
+                } else {
+                    if (open > 0) {
+                        open--;
+                    } else {
+                        int index = q.poll();
+                        sb.setCharAt(index, '(');
+                    }
 
-        if(s.charAt(i) == ')' && count > 0){
-            return dfs(s, i + 1, count - 1, memo);
-        }
-        if(s.charAt(i) == '('){
-            return dfs(s, i + 1, count + 1, memo);
-        }
-        if(s.charAt(i) == '*'){
-            if(count > 0){
-                if(dfs(s, i + 1, count - 1, memo)){
-                    memo[i][count] = true;
-                    return true;
                 }
             }
-            if(dfs(s, i + 1, count + 1, memo)){
-                memo[i][count] = true;
-                return true;
-            }
-            if(dfs(s, i + 1, count, memo)){
-                memo[i][count] = true;
-                return true;
-            }
-
         }
-        memo[i][count] = false;
-        return false;
+        if (open == 0) {
+            return true;
+        }
+        open = 0;
+        q = new LinkedList<>();
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = sb.charAt(i);
+            if (ch == ')') {
+                open++;
+            } else if (ch == '*') {
+                q.add(i);
+            } else {
+                if (open == 0 && q.isEmpty()) {
+                    return false;
+                } else {
+                    if (open > 0) {
+                        open--;
+                    } else {
+                        int index = q.poll();
+                        sb.setCharAt(index, ')');
+                    }
+
+                }
+            }
+        }
+
+        return q.size() >= open;
+
     }
 }
-
